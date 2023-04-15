94e61daf4ba07cbfc50f6e37e331babac22b953b
Matheus Ribeiro <mtr.matheus.ribeiroc@gmail.com>
2023-04-15 12:31:26 -0300

    att

---
+++ Projeto01/arrayLinks.js
@@ -0,0 +1,7 @@
+function geraArrayDeUrl(arrayLinks){
+    return arrayLinks.map(objetoLink => Object.values(objetoLink).join())
+}
+function validaURLArray(arrayLinks){
+    return geraArrayDeUrl(arrayLinks)
+}
+export default validaURLArray;
--- Projeto01/index.js
+++ Projeto01/index.js
@@ -25,7 +25,6 @@ async function pegaArquivo(caminhoDoArquivo){
         return(extraiLinks(texto));
     } catch (erro) {
         trataErro(erro);
-        
     }
 }
 
